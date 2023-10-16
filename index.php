e5cea0177ccd25ea9f97f052ebd7915b1f761a17
0LrNx <FISSSSSS@proton.me>
2023-10-16 12:46:54 +0200

    feat : connexion via PDO et ajout MLD BDD en WinDesign #37 #39

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-    echo 'Coucou ceci est l\'index de base';
+    
+
+    include_once 'php/param_connexion_etu.php';
+    include_once 'php/pdo_agile.php';
+
+    $conn = OuvrirConnexionPDO($dbMySql,$db_username,$db_password);
+
+    echo 'Vosu êtes connecté en tant que : '.$db_username.'<br>';
+
+
 ?>
