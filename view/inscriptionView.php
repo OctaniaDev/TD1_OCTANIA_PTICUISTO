9f0a50653f8cca757fe491c023ab7cbfe2436b2b
0LrNx <FISSSSSS@proton.me>
2023-10-26 16:19:39 +0200

    feat : ajout de la vue d'inscription

---
+++ view/inscriptionView.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Inscription</title>
+</head>
+
+<body>
+    <h1>Inscription</h1>
+
+    <form method="post" action="">
+
+        <label for="username">Nom :</label>
+        <input type="text" id="nom" name="nom" required><br>
+
+        <label for="username">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" required><br>
+
+        <label for="username">Email :</label>
+        <input type="text" id="email" name="email" required><br>
+
+        <label for="username">Pseudo :</label>
+        <input type="text" id="pseudo" name="pseudo" required><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required><br>
+
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+
+</html>
