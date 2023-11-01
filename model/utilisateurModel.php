5e97ac32922f33910e8e1a384036cd81bc725b5c
Paul Bagot <bagotpa@gmail.com>
2023-11-01 19:25:54 +0100

    feat : mdp hasher + fix erreurs

--- model/utilisateurModel.php
+++ model/utilisateurModel.php
@@ -8,23 +8,19 @@ class Utilisateur {
     }
 
     public function connexion($username, $password) {
-        $sql = "SELECT UTI_PSEUDO, UTI_MDP, TYPE_LIBELLE FROM CUI_UTILISATEUR JOIN CUI_TYPE_UTILISATEUR USING(TYPE_iD) WHERE UTI_PSEUDO ='".$username."' AND UTI_MDP ='".$password."'";
+        $sql = "SELECT UTI_PSEUDO, UTI_MDP, TYPE_LIBELLE FROM CUI_UTILISATEUR JOIN CUI_TYPE_UTILISATEUR USING(TYPE_iD) WHERE UTI_PSEUDO ='".$username."' AND UTI_MDP = '". md5($password) ."'";
         LireDonneesPDO1($this->connection,$sql,$tab);
-        /*$hashedPassword = $stmt->fetchColumn();
-    
-        if ($hashedPassword && password_verify($password, $hashedPassword)) {
-            return true;    
-        } else {
-            return false; // Identifiants incorrects
-        }*/
-
         $this->connection = null;
         return $tab;
     }
 
 
     public function inscription($nom, $prenom, $email, $pseudo, $password) {
-        $sql = "INSERT INTO CUI_UTILISATEUR VALUES (null,2,1,'".$password."', '".$pseudo."', '".$email."', '".$prenom."','".$nom."',sysdate())";
+        $sql = "SELECT * FROM CUI_UTILISATEUR WHERE UTI_PSEUDO = '" . $pseudo . "'";
+        LireDonneesPDO1($this->connection,$sql,$tab);
+        if(isset($tab[0]["UTI_PSEUDO"]))
+            return false;
+        $sql = "INSERT INTO CUI_UTILISATEUR VALUES (null,3,1,'". md5($password) ."', '".$pseudo."', '".$email."', '".$prenom."','".$nom."',sysdate())";
         $req = majDonneesPDO($this->connection,$sql);
         $this->connection = null;
         return $req;
