7278a536507a1be490ae92cfaaf985b663f435a0
FI$$ <FISSSSSS@proton.me>
2023-11-08 19:25:18 +0100

    feat : configuration du fichier tailwind

--- view/template.php
+++ view/template.php
@@ -4,7 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $titre ?> </title>
+
+    <!-- Police de titre -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <!-- Police d'écriture -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+
+    <!-- TAILWIND CSS -->
     <link rel="stylesheet" href="./dist/output.css">
+
 </head>
 <body>
 <?php
