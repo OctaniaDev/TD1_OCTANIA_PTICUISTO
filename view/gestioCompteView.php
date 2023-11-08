e9873c602384eeb7d1fd02950f99cb57498974ba
FI$$ <FISSSSSS@proton.me>
2023-11-08 20:45:29 +0100

    fix : nommage fichiers

--- view/gestioCompteView.php
+++
@@ -1,9 +0,0 @@
-<?php ob_start(); ?>
-
-<h1>Gestion de comptes</h1>
-
-<?php
-$content = ob_get_clean();
-$titre = 'Gestion de comptes';
-require $GLOBALS['root'] . 'view/template.php';
-?>
--- view/gestionCompteView.php
+++ view/gestionCompteView.php
@@ -1,26 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion de comptes</title>
-</head>
-<body>
-    <h1>Gestion de comptes</h1>
+<?php ob_start(); ?>
 
-    <?php
-        if (!empty($utilisateurs)) {
-            echo '<ul>';
-                foreach ($utilisateurs as $utilisateur) {
-                    echo '<li><p>'.$utilisateur['UTI_PSEUDO'] . '</p><a href="./index.php?action=voir_utilisateur&user_id='
-                    . $utilisateur['UTI_ID'].
-                    '">voir le profil de l\'utilisateur</a></li>';
-                }
-            echo '</ul>';
-        } else {
-            echo '<p>Aucun utilisateur trouvé</p>';
-        }
-    ?>
+<h1>Gestion de comptes</h1>
 
-</body>
-</html>
+<?php
+$content = ob_get_clean();
+$titre = 'Gestion de comptes';
+require $GLOBALS['root'] . 'view/template.php';
+
+    if (!empty($utilisateurs)) {
+        echo '<ul>';
+            foreach ($utilisateurs as $utilisateur) {
+                echo '<li><p>'.$utilisateur['UTI_PSEUDO'] . '</p><a href="./index.php?action=voir_utilisateur&user_id='
+                . $utilisateur['UTI_ID'].
+                '">voir le profil de l\'utilisateur</a></li>';
+            }
+        echo '</ul>';
+    } else {
+        echo '<p>Aucun utilisateur trouvé</p>';
+    }
+?>
